b940d4f46eeb3be1a0a6c4e50b1048e11663e843
Scott Werner <stwerner@vt.edu>
2013-03-13 00:03:28 -0400

    Proof of concept commit

--- lib/howdoi.rb
+++ lib/howdoi.rb
@@ -1,4 +1,19 @@
 require "howdoi/version"
+require 'nokogiri'
+require 'open-uri'
 
 module Howdoi
+  class Searcher
+    attr_accessor :result
+
+    SITES = {
+      "program" => "http://stackoverflow.com",
+      "sysadmin" => "http://serverfault.com"
+    }
+
+    def initialize(site, search)
+      doc = Nokogiri::HTML(open("#{SITES[site]}/search?q=#{search}"))
+      @result = "#{SITES[site]}#{doc.css(".result-link a").first.attribute("href").value}"
+    end
+  end
 end
